12acacf98c739a6598714c561412ed18edee4dda
j-hagedorn <josh.hagedorn@gmail.com>
2014-08-29 12:22:59 -0400

    Update load file, remove old datasets, add alternate10to13 df

--- 404code/load_open404_data.R
+++ 404code/load_open404_data.R
@@ -1,15 +1,15 @@
-# Load subMasterPlus datafile
+# Load subMaster datafile
 library(RCurl) # requires install.packages("RCurl")
-data <- getURL("https://raw.githubusercontent.com/j-hagedorn/open404/master/data/clean/subMasterPlus",
+data <- getURL("https://raw.githubusercontent.com/j-hagedorn/open404/master/data/clean/subMaster",
                ssl.verifypeer=0L, followlocation=1L)
 writeLines(data,'temp.csv')
-subMasterPlus <- read.csv('temp.csv')
+subMaster <- read.csv('temp.csv')
 
-# Load merged10to12 datafile (with census data for population rates from 2010-2012)
-data <- getURL("https://raw.githubusercontent.com/j-hagedorn/open404/master/data/clean/merged10to12",
+# Load enriched10to13 datafile (with census data for population rates from 2010-2013)
+data <- getURL("https://raw.githubusercontent.com/j-hagedorn/open404/master/data/clean/enriched10to13",
                ssl.verifypeer=0L, followlocation=1L)
 writeLines(data,'temp.csv')
-merged10to12 <- read.csv('temp.csv')
+enriched10to13 <- read.csv('temp.csv')
 
 # Load alt10to12 datafile (with Lifeways corrections for 2012)
 data <- getURL("https://raw.githubusercontent.com/j-hagedorn/open404/master/data/clean/alt10to12",
