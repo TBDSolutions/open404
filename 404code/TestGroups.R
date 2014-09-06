f200a1cdbb3f487c22cc9618331075cea10e78f0
j-hagedorn <josh.hagedorn@gmail.com>
2014-09-05 23:40:07 -0400

    Get lrp_mshn distinct counts

--- 404code/TestGroups.R
+++ 404code/TestGroups.R
@@ -8,8 +8,37 @@ svs_groups <- summarize(group_by(Master, FirstofService.Description, Service),
 svs_groups <- summarize(group_by(Master, FirstOfHCPCS.Code, FirstofService.Description), 
                         n_distinct(FirstofService.Description))
 
-svs_groups <- summarize(group_by(Master, Code_Mod, FirstOfHCPCS.Code), 
-                        n_distinct(FirstOfHCPCS.Code))
+svs_groups <- 
+  Master %>%
+  #filter(FirstofService.Description=='Assessment for Autism') %>%
+  group_by(Service,FirstofService.Description,FirstOfHCPCS.Code) %>%
+  summarize(n_distinct(FirstOfHCPCS.Code)) %>%
+  arrange(Service)
 
 svs_groups <- arrange(svs_groups, FirstOfHCPCS.Code, FirstofService.Description)
 
+
+levels(unique_06to13$CMHSP)
+
+lrp_mshn <-
+unique_06to13 %>%
+  filter(FY==2013, 
+         CMHSP=="Allegan"|
+         CMHSP=="Muskegon"|
+         CMHSP=="Network180"|
+         CMHSP=="Ottawa"|
+         CMHSP=="West Michigan"|
+         CMHSP=="Bay-Arenac"|
+         CMHSP=="CMH for Central Michigan"|
+         CMHSP=="Clinton Eaton Ingham"|
+         CMHSP=="Gratiot"|
+         CMHSP=="Huron"|
+         CMHSP=="Ionia"|
+         CMHSP=="Lifeways"|
+         CMHSP=="Montcalm"|
+         CMHSP=="Newaygo"|
+         CMHSP=="Saginaw"|
+         CMHSP=="Shiawassee"|
+         CMHSP=="Tuscola") %>%
+  summarize(served=sum(TotalServed))
+  
