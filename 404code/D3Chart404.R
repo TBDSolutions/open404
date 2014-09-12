2bcba72a499faf66626f90503b90afbfc03992c6
j-hagedorn <josh.hagedorn@gmail.com>
2014-09-12 17:49:39 -0400

    Update alt10to13 dataset, create D3 charts

--- 404code/D3Chart404.R
+++ 404code/D3Chart404.R
@@ -39,6 +39,9 @@ hosp2 <- enriched10to13 %>%
 
 require(rCharts)
 
+
+# Multibar chart
+
 n1 <- nPlot(Cost1kSvd ~ Service, 
             group = "PIHPname", 
             data = hosp2, 
@@ -61,15 +64,74 @@ hosp3 <- subMaster %>%
     #CostPerPerson = round((sum(SumOfCost)/sum(SumOfCases)),digits=2),
     #CostPerUnit = round((sum(SumOfCost)/sum(SumOfUnits)),digits=2),
     #CostPUPM = round((sum(SumOfCost)/sum(SumOfCases)/12),digits=2),
-    Cost1kSvd = round((sum(SumOfCost)/sum(TotalServed)*1000), digits = 2)#,
-    #Unit1kSvd = round((sum(SumOfUnits)/sum(TotalServed)*1000), digits = 1),
-    #Perc_Svd = round((sum(SumOfCases)/sum(TotalServed)*100), digits = 1),
-    #Cost1kPop = round(sum(SumOfCost)/(sum(subPop)/1000),digits = 2),
-    #Srvd1kPop = round(sum(SumOfCases)/(sum(subPop)/1000),digits = 1)
+    #Cost1kSvd = round((sum(SumOfCost)/max(TotalServed)*1000), digits = 2)#,
+    #Unit1kSvd = round((sum(SumOfUnits)/max(TotalServed)*1000), digits = 1),
+    Perc_Svd = round((sum(SumOfCases)/min(TotalServed)*100), digits = 1)#,
+    #Cost1kPop = round(sum(SumOfCost)/(max(subPop)/1000),digits = 2),
+    #Srvd1kPop = round(sum(SumOfCases)/(max(subPop)/1000),digits = 1)
   ) 
 
-n2 <- nPlot(Cost1kSvd ~ FY, 
+n2 <- nPlot(Perc_Svd ~ FY, 
             group = "CMHSP",
             data = hosp3, 
-            type = 'lineChart')
+            type = 'lineChart') # OR 'lineWithFocusChart'
+n2$yAxis(axisLabel = "% served in local psych hosp", width = 62)
+n2$xAxis(axisLabel = "Year")
 n2
+n2$save('LocalPsychHosp06to13.html', standalone = TRUE)
+
+# Scatter Chart
+
+hosp4 <- enriched10to13 %>%
+  filter(FY==2013 & Service=="Local Hospitalization") %>%
+  mutate(CMH_Disability = as.factor(paste(CMHSP,Population, sep = "-"))) %>%
+  group_by(CMH_Disability,Population,Service) %>%
+  select(PIHPname,Service,SumOfUnits,SumOfCases,SumOfCost,TotalServed,subPop) %>%
+  summarise(#UnitPerPerson = round((sum(SumOfUnits)/sum(SumOfCases)),digits=1),
+    #CostPerPerson = round((sum(SumOfCost)/sum(SumOfCases)),digits=2),
+    CostPerUnit = round((sum(SumOfCost)/sum(SumOfUnits)),digits=2),
+    #CostPUPM = round((sum(SumOfCost)/sum(SumOfCases)/12),digits=2),
+    #Cost1kSvd = round((sum(SumOfCost)/sum(TotalServed)*1000), digits = 2)#,
+    #Unit1kSvd = round((sum(SumOfUnits)/sum(TotalServed)*1000), digits = 1),
+    Perc_Svd = round((sum(SumOfCases)/sum(TotalServed)*100), digits = 1)#,
+    #Cost1kPop = round(sum(SumOfCost)/(sum(subPop)/1000),digits = 2),
+    #Srvd1kPop = round(sum(SumOfCases)/(sum(subPop)/1000),digits = 1)
+  )
+
+n3 <- nPlot(Perc_Svd ~ CostPerUnit, group = "Population", data = hosp4, type = 'scatterChart')
+n3$xAxis(axisLabel = 'Cost per day for local psych hosp, 2013', width = 62)
+n3$yAxis(axisLabel = '% beneficiaries using local psych hosp, 2013', width = 62)
+n3
+
+n3$save('UnitCostVsPercUse.html', standalone = TRUE)
+
+# Multi Chart
+
+hosp5 <- subMaster %>%
+  filter(Service=="Local Hospitalization") %>%
+  mutate(CMH_Disability = as.factor(paste(CMHSP,Population, sep = "-"))) %>%
+  group_by(FY,PIHPname,CMH_Disability,Population,Service) %>%
+  select(PIHPname,Service,SumOfUnits,SumOfCases,SumOfCost,TotalServed) %>%
+  summarise(UnitPerPerson = round((sum(SumOfUnits)/sum(SumOfCases)),digits=1),
+    #CostPerPerson = round((sum(SumOfCost)/sum(SumOfCases)),digits=2),
+    CostPerUnit = round((sum(SumOfCost)/sum(SumOfUnits)),digits=2),
+    #CostPUPM = round((sum(SumOfCost)/sum(SumOfCases)/12),digits=2),
+    #Cost1kSvd = round((sum(SumOfCost)/sum(TotalServed)*1000), digits = 2)#,
+    #Unit1kSvd = round((sum(SumOfUnits)/sum(TotalServed)*1000), digits = 1),
+    Perc_Svd = round((sum(SumOfCases)/sum(TotalServed)*100), digits = 1)#,
+    #Cost1kPop = round(sum(SumOfCost)/(sum(subPop)/1000),digits = 2),
+    #Srvd1kPop = round(sum(SumOfCases)/(sum(subPop)/1000),digits = 1)
+  )
+
+
+n4 <- nPlot(Perc_Svd ~ FY, group = 'PIHPname', data = hosp5, type = 'multiChart')
+n4$set(multi = list(
+  UnitPerPerson = list(type="area", yAxis=1),
+  CostPerUnit = list(type="line", yAxis=2)
+))
+n4$setTemplate(script = system.file(
+  "/libraries/nvd3/layouts/multiChart.html",
+  package = "rCharts"
+))
+n4
+
