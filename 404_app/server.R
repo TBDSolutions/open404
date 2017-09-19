1bcc58010fae19b3a8593c3d120bcf33195d6232
Joshh <josh.hagedorn@gmail.com>
2017-09-19 14:12:04 -0400

    Fix variable matching and renaming

--- 404_app/server.R
+++ 404_app/server.R
@@ -1,32 +1,36 @@
 # server.R #
 
-server <- function(input, output) {
+shinyServer(function(input, output) {
   
   #### Make Reactive Datasets ####
 
   df <- reactive({
 
-df <-  data404 %>%
-      select(FY,PIHPname,PIHP,CMHSP,Population,ServiceType
-             ,Service,Description,Code,Code_Mod
-             ,contains(input$x),contains(input$y)
-      ) %>%
-      rename(x = input$x,
-             y = input$y)
+    data404 %>%
+      select(
+        FY,PIHPname,PIHP,CMHSP,Population,ServiceType,
+        Service,Description,Code,Code_Mod,
+        x = matches(input$x),
+        y = matches(input$y)
+      ) 
 
   })
   
   #### Visualizations ####
   
   output$bubble <- renderPlotly({
-    df %>%
-      filter(
-        FY == input$sliderFY) %>%
-    plot_ly(x = ~x, y = ~input$y, type = 'scatter', 
-            mode = 'markers', marker = list(opacity = 0.5)) %>%
-      layout(title = '',
-             xaxis = list(showgrid = FALSE),
-             yaxis = list(showgrid = FALSE))
+    df() %>%
+      filter(FY == input$sliderFY) %>%
+      plot_ly(
+        x = ~x, y = ~y, type = 'scatter', 
+        mode = 'markers', marker = list(opacity = 0.5)
+      ) %>%
+      layout(
+        title = '',
+        xaxis = list(showgrid = FALSE),
+        yaxis = list(showgrid = FALSE)
+      )
+    
   })
 
-}
+})
