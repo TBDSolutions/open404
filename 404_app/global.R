be74879d099e05ce146c74ff0d26aa669cd9dcc5
jenna-dietsch <30694833+jenna-dietsch@users.noreply.github.com>
2017-12-27 12:54:01 -0500

    Update Color Palette

--- 404_app/global.R
+++ 404_app/global.R
@@ -69,11 +69,20 @@ inputs_sub <-
 
 #### Colors ####
 
-cmh_palette <- c("#a3ab73","#4e64ed","#b6d325","#971da8","#66ad00","#fb79fd",
-                 "#009721","#f22a9b","#9ed75a","#7630a1","#6f9100","#ad7cff",
-                 "#fda812","#0071da","#c69e00","#7a93ff","#8f8e00","#b0008a",
-                 "#86d88f","#ab0066","#009266","#f62f51","#02c6ee","#c8081d",
-                 "#62d8d4","#cf4900","#334da2","#cbcb67","#ff8de3","#50571e",
-                 "#e6a5ff","#a26700","#64467f","#e0c476","#c60052","#ffaa79",
-                 "#ff94cb","#9a4400","#bc7ea0","#982b1e","#ffa6b2","#774723",
-                 "#ff7a6f","#893b26","#c1798a","#a3615b","#9844bd")
+# library(RColorBrewer)
+# n <- 47
+# qual_col_pals = brewer.pal.info[brewer.pal.info$category == 'qual',]
+# cmh_palette = unlist(mapply(brewer.pal, qual_col_pals$maxcolors, rownames(qual_col_pals)))
+
+library(randomcoloR)
+n <- 47
+cmh_palette <- distinctColorPalette(n)
+
+# cmh_palette <- c("#a3ab73","#4e64ed","#b6d325","#971da8","#66ad00","#fb79fd",
+#                  "#009721","#f22a9b","#9ed75a","#7630a1","#6f9100","#ad7cff",
+#                  "#fda812","#0071da","#c69e00","#7a93ff","#8f8e00","#b0008a",
+#                  "#86d88f","#ab0066","#009266","#f62f51","#02c6ee","#c8081d",
+#                  "#62d8d4","#cf4900","#334da2","#cbcb67","#ff8de3","#50571e",
+#                  "#e6a5ff","#a26700","#64467f","#e0c476","#c60052","#ffaa79",
+#                  "#ff94cb","#9a4400","#bc7ea0","#982b1e","#ffa6b2","#774723",
+#                  "#ff7a6f","#893b26","#c1798a","#a3615b","#9844bd")
